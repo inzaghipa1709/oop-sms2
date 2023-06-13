45e19b4e4fc84161f8def113a91258db13005fad
Inzaghi's Dev <108411478+inzaghipa1709@users.noreply.github.com>
2023-06-13 12:00:34 +0700

    Add files via upload

---
+++ OOPServer1/OOPServer1/Controllers/MahasiswaController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OopServer1.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class MahasiswaController : Controller
+    {
+
+        public MahasiswaController()
+        {
+        }
+
+        [HttpPost]
+        [Route("AddStudent")]
+        public Mahasiswa AddStudent([FromBody] Mahasiswa dataClient)
+        {
+            dataClient.Id = 2222;
+            dataClient.Nama = "Namamu sudah saya rubah ya...";
+            return dataClient;
+        }
+
+
+        [HttpGet]
+        [Route("GetMahasiswas")]
+        public IList<Mahasiswa> GetMahasiswas()
+        {
+            var mhss = new List<Mahasiswa>() { new Mahasiswa() { Email="hhhh", Nama="jjjjj" }, new Mahasiswa() { Email = "CCCC", Nama = "aaaa" } };
+            return mhss;
+        }
+
+
+    }
+}
---
+++ OOPServer1/OOPServer1/Models/Mahasiswa.cs
@@ -0,0 +1,9 @@
+namespace OopServer1
+{
+    public class Mahasiswa
+    {
+        public int Id { get; set; }
+        public string Nama { get; set; }
+        public string Email { get; set; }
+    }
+}
