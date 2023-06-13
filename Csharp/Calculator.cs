b6a94fadb3406cef4af4b691a80b42c6ffa2ff23
Inzaghi's Dev <108411478+inzaghipa1709@users.noreply.github.com>
2023-06-13 11:51:23 +0700

    Add files via upload

---
+++ Csharp/Calculator.cs
@@ -0,0 +1,54 @@
+﻿//using System;
+
+//namespace Csharp{
+//    public class Calculator{
+//        public class CalculatorService
+//        {
+//            public double A { get; set; }
+//            public double B { get; set; }
+
+//            public double Add()
+//            {
+//                return A + B;
+//            }
+
+//            public double Subtract()
+//            {
+//                return A - B;
+//            }
+
+//            public double Multiply()
+//            {
+//                return A * B;
+//            }
+//            public double Divide()
+//            {
+//                return A / B;
+//            }
+//        }
+
+//        public static void Main(string[] args){
+//            CalculatorService calculatorService = new CalculatorService();
+//            Console.WriteLine("Output :");
+
+//            calculatorService.A = 4.5;
+//            calculatorService.B = 1.3;
+
+//            Console.WriteLine(calculatorService.A);
+//            Console.WriteLine(calculatorService.B);
+//
+//            double hasilPenjumlahan = calculatorService.Add();
+//            Console.WriteLine("\nHasil Penjumlahan :");
+//            Console.WriteLine(hasilPenjumlahan);
+//            double hasilPengurangan = calculatorService.Subtract();
+//            Console.WriteLine("\nHasil Pengurangan :");
+//            Console.WriteLine(hasilPengurangan);
+//            double hasilKali = calculatorService.Multiply();
+//            Console.WriteLine("\nHasil Perkalian :");    
+//            Console.WriteLine(hasilKali);
+//            double hasilBagi = calculatorService.Divide();
+//            Console.WriteLine("\nHasil Pembagian :");    
+//            Console.WriteLine(hasilBagi);
+//        }
+//    }
+//}
---
+++ Csharp/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace Csharp{
+    public class Program{
+        public class CalculatorService //Class
+        {
+            public double A { get; set; } //Property
+            public double B { get; set; } //Property
+
+            public double Add() //Method
+            {
+                return A + B;
+            }
+
+            public double Subtract() //Method
+            {
+                return A - B;
+            }
+
+            public double Multiply() //Method
+            {
+                return A * B;
+            }
+            public double Divide() //Method
+            {
+                return A / B;
+            }
+        }
+
+    class Kendaraan  // Base class
+    {
+        public string brand = "Honda";  // Field
+        public void honk()             // Method 
+        {
+            Console.WriteLine("Brem, brem!");
+        }
+        class Mobil : Kendaraan  // Derived class
+        {
+            public string modelName = "Mobilio";  // Field
+        }
+    }
+
+        public static void Main(string[] args){
+            CalculatorService calculatorService = new CalculatorService(); //Constructor
+            Console.WriteLine("Output :"); //Object: calculatorService
+
+            calculatorService.A = 4.5; //Field
+            calculatorService.B = 1.3; //Field
+
+            Console.WriteLine(calculatorService.A);
+            Console.WriteLine(calculatorService.B);
+
+            double hasilPenjumlahan = calculatorService.Add();
+            Console.WriteLine("\nHasil Penjumlahan :");
+            Console.WriteLine(hasilPenjumlahan);
+            double hasilPengurangan = calculatorService.Subtract();
+            Console.WriteLine("\nHasil Pengurangan :");
+            Console.WriteLine(hasilPengurangan);
+            double hasilKali = calculatorService.Multiply();
+            Console.WriteLine("\nHasil Perkalian :");    
+            Console.WriteLine(hasilKali);
+            double hasilBagi = calculatorService.Divide();
+            Console.WriteLine("\nHasil Pembagian :");    
+            Console.WriteLine(hasilBagi);
+
+            //Membuat Objek
+            Mobil myCar = new Mobil();
+            //Memanggil method honk() (Dari Class Kendaraan) dalam objek myCar
+            myCar.honk();
+            //Menampilkan value dari brand field (Dari Class Kendaraan) dan juga nilai modelName dari kelas Mobil
+            Console.WriteLine(myCar.brand + " " + myCar.modelName);
+        }
+    }
+}
